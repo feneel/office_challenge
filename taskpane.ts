56f8a165ae9d3473afd8e601b47dd3dc17a6ae80
feneel <feneeldoshi@gmail.com>
2025-12-24 15:15:26 -0500

    Adding Connect UI to taskpane

---
+++ taskpane.ts
@@ -0,0 +1,13 @@
+/// <reference types="office-js">
+
+console.log(" Executing taskpane!!!")
+
+function setStatus(msg: string){
+    const element = document.getElementById("status-text")
+    if (element) element.textContent = msg
+}
+
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    setStatus("Ready. Click Connect!!")
+})
